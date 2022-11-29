bfcb14fbbfeec12e4818769b7463f74adcc6ca86
Konkin <konkin.ivan@yandex.ru>
2022-11-29 18:37:02 +0300

    My Array Filter

--- array_filter/my_array_filter.php
+++ array_filter/my_array_filter.php
@@ -1,6 +1,6 @@
 <?php
 
-$arr = [-10, -9, -8, -7, -6, -5, -4 -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$arr = [-9, -8, -7, -6, -5, -4 -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function my_array_filter(array $array, ?callable $callback = null, int $mode = 0): array
 {
